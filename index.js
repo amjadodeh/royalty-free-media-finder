84a51ddb81467937307412564173f6610d2f0e5e
Amjad Odeh <aao001056485@gmail.com>
2020-09-27 02:09:36 -0400

    audio search option is fully functional

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 'use strict';
 
-const audioApiKey = 'HvhIQH3WHGMQoU37KAwlU6PG4p4O7M3eVjRvufax';
-const imageSearchURL = 'https://api.pexels.com/v1/search';
-const videoSearchURL = 'https://api.pexels.com/videos/search';
+const photoSearchURL = 'https://api.pexels.com/v1/search?query=';
+const videoSearchURL = 'https://api.pexels.com/videos/search?query=';
 const audioSearchURL = 'https://freesound.org/apiv2/search/text/';
 
+const audioApiKey = 'HvhIQH3WHGMQoU37KAwlU6PG4p4O7M3eVjRvufax';
+
 var fetchSettings = {
-  url: 'https://api.pexels.com/v1/search?query=',
+  url: '',
   method: 'GET',
   timeout: 0,
   headers: {
     Authorization: '563492ad6f91700001000001e6dc0d922c664b3b995eedd2333b7b68',
   },
 };
+
+function fetchImage() {
+  $.ajax(fetchSettings).done(function (response) {
+    console.log(response);
+  });
+}
+
+/********** TEMPLATE GENERATION FUNCTIONS **********/
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params).map(
@@ -21,34 +30,67 @@ function formatQueryParams(params) {
   return queryItems.join('&');
 }
 
-function displayResults(responseJson) {
+function fetchAudioResults(url) {
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then((responseJson) => displayAudioResults(responseJson))
+    .catch((err) => {
+      $('#results-list').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+/********** RENDER FUNCTION **********/
+
+function displayAudioResults(responseJson) {
   console.log(responseJson);
   $('#results-list').empty();
   for (let i = 0; i < responseJson.results.length; i++) {
     $('#results-list').append(
       `<li><h3>${responseJson.results[i].name}</h3>
+      <img src="${responseJson.results[i].images.waveform_m}" alt="waveform image">
+      <br>
+      <audio controls>
+      <source src="${responseJson.results[i].previews['preview-lq-mp3']}" type="audio/mpeg">
+      Preview unavailable. Your browser does not support the audio element.
+      </audio>
       <p>${responseJson.results[i].description}</p>
       <a href='${responseJson.results[i].download}'>Download Here</a>
       </li>`
     );
   }
+  if (responseJson.previous == null) {
+    $('#results-list').append(
+      `
+      <br>
+      <br>
+      <a class='next' href='${responseJson.next}'>Next</a>
+      `
+    );
+  } else {
+    $('#results-list').append(
+      `
+      <br>
+      <br>
+      <a class='prev' href='${responseJson.previous}'>Previous</a>
+      <br>
+      <a class='next' href='${responseJson.next}'>Next</a>
+      `
+    );
+  }
   $('#results').removeClass('hidden');
 }
 
-function fetchImage() {
-  $.ajax(imageFetchSettings).done(function (response) {
-    console.log(response);
-  });
-}
+/********** SINGLE PURPOSE FUNCTIONS **********/
 
-function getResults(type, input) {
+function submitInputs(type, input) {
   if (type === 'Photo') {
-    console.log('Photo');
   } else if (type === 'Video') {
-    console.log('Video');
   } else if (type === 'Audio') {
-    console.log('Audio');
-
     const params = {
       token: audioApiKey,
       fields: 'id,name,description,previews,download,images',
@@ -60,18 +102,25 @@ function getResults(type, input) {
 
     console.log(url);
 
-    fetch(url)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error(response.statusText);
-      })
-      .then((responseJson) => displayResults(responseJson))
-      .catch((err) => {
-        $('#results-list').text(`Something went wrong: ${err.message}`);
-      });
+    fetchAudioResults(url);
   }
+}
+
+/********** EVENT HANDLER FUNCTIONS **********/
+
+function watchPrevNext() {
+  $('#results-list').on('click', '.prev', function (event) {
+    event.preventDefault();
+    const url = $('.prev').attr('href') + '&token=' + audioApiKey;
+    fetchAudioResults(url);
+    window.location.hash = 'main-div';
+  });
+  $('#results-list').on('click', '.next', function (event) {
+    event.preventDefault();
+    const url = $('.next').attr('href') + '&token=' + audioApiKey;
+    fetchAudioResults(url);
+    window.location.hash = 'main-div';
+  });
 }
 
 function watchForm() {
@@ -79,8 +128,9 @@ function watchForm() {
     event.preventDefault();
     const mediaType = $('#js-media-type').val();
     const searchInput = $('#js-search-input').val();
-    getResults(mediaType, searchInput);
+    submitInputs(mediaType, searchInput);
   });
 }
 
 $(watchForm);
+$(watchPrevNext);
